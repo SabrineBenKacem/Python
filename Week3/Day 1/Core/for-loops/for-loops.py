9f1ad35d91316be522f35b06db09660133135a92
Sabrine Ben Kacem <sabrinebenkacem@gmail.com>
2023-07-16 23:07:14 +0100

    for-loops

---
+++ Week3/Day 1/Core/for-loops/for-loops.py
@@ -0,0 +1,26 @@
+for i in range(0,151):
+    print (i)
+for j in range(5,1001):
+    if (j%5 ==0):
+        print(j)
+for i in range(1,101):
+    if (i%10 ==0):
+        print("Coding Dojo")
+    elif (i%5 ==0):
+        print("Coding")
+    else:
+        print(i)
+sum=0
+for i in range(0,500001):
+    if (i%2 !=0):
+        sum+=i
+print(sum)
+for i  in range(2018,0,-4):
+    print(i)
+lowNum=10
+highNum=250
+mult=7
+for i in range(lowNum,highNum+1):
+    if (i%mult==0):
+        print(i)
+
